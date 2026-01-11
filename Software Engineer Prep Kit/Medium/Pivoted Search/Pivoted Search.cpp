08ffd3e07874c229e60168523e1b88de824ad912
JaretEduardo <jaret.gonzalez.ct59@gmail.com>
2026-01-10 21:11:37 -0600

    Pivoted Search

---
+++ Software Engineer Prep Kit/Medium/Pivoted Search/Pivoted Search.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+int searchRotatedTimestamps(vector<int> nums, int target) {
+    int left = 0;
+    int right = nums.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        if (nums[left] <= nums[mid]) {
+            if (target >= nums[left] && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            if (target > nums[mid] && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    string nums_count_temp;
+    getline(cin, nums_count_temp);
+
+    int nums_count = stoi(ltrim(rtrim(nums_count_temp)));
+
+    vector<int> nums(nums_count);
+
+    for (int i = 0; i < nums_count; i++) {
+        string nums_item_temp;
+        getline(cin, nums_item_temp);
+
+        int nums_item = stoi(ltrim(rtrim(nums_item_temp)));
+
+        nums[i] = nums_item;
+    }
+
+    string target_temp;
+    getline(cin, target_temp);
+
+    int target = stoi(ltrim(rtrim(target_temp)));
+
+    int result = searchRotatedTimestamps(nums, target);
+
+    cout << result << "\n";
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
