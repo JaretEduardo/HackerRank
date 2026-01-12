934e157670697a0764c19868a01d2241622d1e3c
JaretEduardo <jaret.gonzalez.ct59@gmail.com>
2026-01-12 17:00:28 -0600

    For Loop

---
+++ C++/For Loop/For Loop.cpp
@@ -0,0 +1,46 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+int main() {
+    int a, b;
+    
+    cin >> a >> b;
+    
+    string numers[] = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    
+    for (int i = a; i <= b; i++)
+    {
+        if (i >= 1 && i <= 9){
+            cout << numers[i] << endl;
+        }
+        else if ((i & 1) == 0){
+            cout << "even" << endl;
+        }
+        else{
+            cout << "odd" << endl;
+        }
+    }
+    
+    return 0;
+}
